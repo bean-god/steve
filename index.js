654f5d2388dc954f7508b58b3dd43826c03cfa0b
bean-god <70300912+bean-god@users.noreply.github.com>
2020-09-01 14:21:42 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -18,45 +18,77 @@ msg.reply(`spoon`)
 }
 }) */
 client.on("message", msg => {
-	if (msg.content.includes("when is the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
-    if (msg.content.includes("when's the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
-    if (msg.content.includes("When is the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
-    if (msg.content.includes("When's the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
-    if (msg.content.startsWith("Hello")) {msg.reply(`How may I help you today?`)}
-    if (msg.content.startsWith("Steve")) {msg.reply(`How may I help you today?`)}
+   if (msg.content.includes("when is the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
+   if (msg.content.includes("when's the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
+   if (msg.content.includes("When is the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
+   if (msg.content.includes("When's the stream")) {msg.reply(`We are unable to reach Customer Services right now. Please try again later.`)}
+ })
+client.on("message", msg => {
+  if (msg.content.includes("where is Bob")) {msg.reply(`You do not need to know.`)}
+  if (msg.content.includes("where is bob")) {msg.reply(`You do not need to know.`)}
+  if (msg.content.includes("Where is Bob")) {msg.reply(`You do not need to know.`)}
+  if (msg.content.includes("Where is bob")) {msg.reply(`You do not need to know.`)}
+})
+client.on("message", msg => {
+  if (msg.content.includes("where is Jake")) {
+  msg.reply(`You do not need to know.`)
+  msg.delete()
+  .catch(console.error);}
+  if (msg.content.includes("where is jake")) {
+  msg.reply(`You do not need to know.`)
+  msg.delete()
+  .catch(console.error);}
+  if (msg.content.includes("Where is Jake")) {
+  msg.reply(`You do not need to know.`)
+  msg.delete()
+  .catch(console.error);}
+  if (msg.content.includes("Where is jake")) {
+  msg.reply(`You do not need to know.`)
+  msg.delete()
+  .catch(console.error);}
+})
+  client.on("message", msg => { 
+  var z = Math.floor(Math.random() * 5)+1;
+  if (msg.content.includes("steve")) {
+  if(z == 1) {msg.reply(`Welcome to Customer Service. My name is Steve. How can I help you?`)}
+  if(z == 2) {msg.reply(`Good afternoon. You’re speaking to Steve. How may I help you?`)}
+  if(z == 3) {msg.reply(`Hi! You’re through to Steve, how can I help you?`)}
+  if(z == 4) {msg.reply(`Hi! Steve speaking, how may I help you today?`)}
+  if(z == 5) {msg.reply(`Steve speaking, what seems to be the issue?`)}
+  }
   })
   client.on("message", msg => {
-  var x = Math.floor(Math.random() * 100)+1;  
-  if(x == 5) {
+  var x = Math.floor(Math.random() * 150)+1;  
+  if(x == 102) {
   msg.reply(`Believe in yourself. Under-confidence leads to a self-fulfilling prophecy that you are not good enough for your work.`)
-  }
+  } else
   if(x == 14) {
   msg.reply(`A better understanding of usage can aid in prioritizing future efforts we're ahead of the curve on that one five-year strategic plan, sorry i was triple muted.`)
-  }
+  } else
   if(x == 38) {
   msg.reply(`Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water.`)
-  }
+  } else
   if(x == 41) {
-  msg.reply(`Jazz it up a little could you rotate the picture to show the other side of the room?`)
-  }
-  if(x == 31) {
+  msg.reply(`Learn to accept everything about yourself, even the part of yourself that struggles to accept everything about yourself`)
+  } else
+  if(x == 61) {
   msg.reply(`Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you.`)
-  }
+  } else
   if(x == 16) {
-  msg.reply(`We have to leverage up the messaging. Deliverables future-proof, so performance review. Bob called an all-hands this afternoon highlights .`)
-  }
+  msg.reply(`Never lose hope. Storms make people stronger and never last forever.`)
+  } else
   if(x == 6) {
   msg.reply(`Do not let your negative thoughts have power over you because those thoughts will end up controlling your life. No one can live a positive life with a negative mind.`)
-  }
-  if(x == 42) {
+  } else
+  if(x == 92) {
   msg.reply(`What does this mean?`)
-  }
+  } else
   if(x == 32) {
   msg.reply(`I hope the council doesn't hear of this.`)
   }
   })
   client.on("message", msg => { 
-   var y = Math.floor(Math.random() * 4)+1;
-  if (msg.content.startsWith("yes or no")) {
-    if (y >= 3) {msg.reply(`yes`)}else {msg.reply(`no`)}
+   var y = Math.floor(Math.random() * 2);
+  if (msg.content.includes("yes or no")) {
+    if (y == 1) {msg.reply(`yes`)}else {msg.reply(`no`)}
   }})

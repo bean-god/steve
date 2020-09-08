777eb0a894eb00812a8ffda60ffa4f39b7066ad3
bean-god <beengbean7@gmail.com>
2020-09-08 11:47:35 -0700

    ;k;jjilgu kyg kfy

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,27 @@
+module.exports = (client, member) => {
+var x = Math.floor(Math.random() * 8)+1;  
+  if(x == 1) {
+  member.send(`Believe in yourself. Under-confidence leads to a self-fulfilling prophecy that you are not good enough for your work.`)
+  } else
+  if(x == 2) {
+  member.send(`A better understanding of usage can aid in prioritizing future efforts we're ahead of the curve on that one five-year strategic plan, sorry i was triple muted.`)
+  } else
+  if(x == 3) {
+  member.send(`Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water.`)
+  } else
+  if(x == 4) {
+  member.send(`Learn to accept everything about yourself, even the part of yourself that struggles to accept everything about yourself`)
+  } else
+  if(x == 5) {
+  member.send(`Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you.`)
+  } else
+  if(x == 6) {
+  member.send(`Never lose hope. Storms make people stronger and never last forever.`)
+  } else
+  if(x == 7) {
+  member.send(`Do not let your negative thoughts have power over you because those thoughts will end up controlling your life. No one can live a positive life with a negative mind.`)
+  } else
+  if(x == 8) {
+  member.send(`I hope the council doesn't hear of this.`)
+  }
+}
